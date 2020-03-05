6d1b2d3792706144884a0615379a1182e69f87a8
florin-smeu <florin.ion.smeu@gmail.com>
2020-03-05 08:15:39 +0200

    5.3.20 Updated server stats methods

--- server/server.py
+++ server/server.py
@@ -83,9 +83,27 @@ def sign_up():
 
 """ ########################### Expenses API ###########################@  """
 
+def compute_average(records):
+	computed_sum = 0
+	for i in range(len(records)):
+		computed_sum += records[i][2]
+	return (float) computed_sum / len(records)
+
+
 @app.route('/average_daily_expenses')
 def average_daily_expenses():
-	return 'No data'
+	"""Method that returns the average income sum for a user per day.
+	"""
+	command = 'SELECT * FROM expenses WHERE username = {}'.format()
+	# records = query_database(command)
+	
+	# TODO Remove once the database is created
+	records = None
+	
+	if records not None:
+		average = compute_average(records)
+	else:
+		return 'No data'
 
 
 @app.route('/average_weekly_expenses')
@@ -100,18 +118,55 @@ def average_monthly_expenses():
 
 @app.route('/daily_detailed_expenses')
 def daily_detailed_expenses():
-	return 'No data'
+	"""Method that returns the expenses detailed per day for a user.
+	"""
+	command = 'SELECT * FROM expenses WHERE username = {}'.format()
+	# records = query_database(command)
+	
+	# TODO Remove once the database is created
+	records = None
+	
+	if records not None:
+		return records
+	else:
+		return 'No data'
 
 
 @app.route('/expenses_between_dates')
 def expenses_between_dates():
-	return 'No data'
+	"""Method that returns the expenses that happened between two dates.
+	"""
+	date1 = request.args.get('date1')
+	date2 = request.args.get('date2')
+
+	command = 'SELECT * FROM expenses WHERE username = {} AND \
+	expense_date BETWEEN {} AND {}'.format(username, date1, date2)
+	# records = query_database(command)
+	
+	# TODO Remove once the database is created
+	records = None
+	
+	if records not None:
+		return records
+	else:
+		return 'No data'
 
 """ ########################## Incomes API #############################@  """
 
 @app.route('/average_daily_incomes')
 def average_daily_incomes():
-	return 'No data'
+	"""Method that returns the average income sum for a user per day.
+	"""
+	command = 'SELECT * FROM incomes WHERE username = {}'.format()
+	# records = query_database(command)
+	
+	# TODO Remove once the database is created
+	records = None
+	
+	if records not None:
+		average = compute_average(records)
+	else:
+		return 'No data'
 
 
 @app.route('/average_weekly_incomes')
@@ -126,12 +181,38 @@ def average_monthly_incomes():
 
 @app.route('/daily_detailed_incomes')
 def daily_detailed_incomes():
-	return 'No data'
+	"""Method that returns the incomes detailed per day for a user.
+	"""
+	command = 'SELECT * FROM incomes WHERE username = {}'.format()
+	# records = query_database(command)
+	
+	# TODO Remove once the database is created
+	records = None
+	
+	if records not None:
+		return records
+	else:
+		return 'No data'
 
 
 @app.route('/incomes_between_dates')
 def incomes_between_dates():
-	return 'No data'
+	"""Method that returns the incomes that happened between two dates.
+	"""
+	date1 = request.args.get('date1')
+	date2 = request.args.get('date2')
+
+	command = 'SELECT * FROM incomes WHERE username = {} AND \
+	income_date BETWEEN {} AND {}'.format(username, date1, date2)
+	# records = query_database(command)
+	
+	# TODO Remove once the database is created
+	records = None
+	
+	if records not None:
+		return records
+	else:
+		return 'No data'
 
 
 @app.route('/')
